d62aee26f5032354f0a9524b82e41f971730de63
Phiip Vierhus <pvierhus@gmail.com>
2015-10-06 14:01:31 -0700

    Home and show page are working together. Need to implement the actual word checking

--- wladder/app/controllers/home_controller.rb
+++ wladder/app/controllers/home_controller.rb
@@ -1,6 +1,24 @@
+load "#{Rails.root}/lib/ladder.rb"
+
 class HomeController < ApplicationController
 
   def index
-    # don't need to do anything here
+    begin
+      @start_word = dictionary.sample
+      @end_word = dictionary.sample
+    end while(@start_word == @end_word)
   end
+
+  def show
+    @words = Array.new
+    @words.push(params[:word1])
+    @words.push(params[:word2])
+    @words.push(params[:word3])
+    @words.push(params[:word4])
+    @words.push(params[:word5])
+    @words.delete_if { |a| a == ""}
+    @start_word = params["start_word"]
+    @end_word = params["end_word"]
+  end
+
 end
--- wladder/config/routes.rb
+++ wladder/config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
 
   root "home#index"
 
+  get 'check', to: 'home#show'
+
 end
