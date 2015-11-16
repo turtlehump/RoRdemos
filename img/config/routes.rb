6f04e9a9aa5556495206297ee232a15ecc25727e
Philip Vierhus <pvierhus@gmail.com>
2015-11-16 12:49:49 -0800

    fixed a typo in the routes

--- img/config/routes.rb
+++ img/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :images do
     resources :tags, shallow: true
-    resources :iage_users, shallow: true
+    resources :image_users, shallow: true
   end
 
   root 'images#index'
